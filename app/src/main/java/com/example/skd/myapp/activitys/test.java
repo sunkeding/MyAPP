de17097a8399747bd56a6cc47d63af330c1a62b6
keding <keding@leoao.com>
2017-05-08 11:33:12 +0800

    删除无用的类

--- app/src/main/java/com/example/skd/myapp/activitys/test.java
+++
@@ -1,8 +0,0 @@
-package com.example.skd.myapp.activitys;
-
-/**
- * Created by skd on 2017/3/22.
- */
-
-public class test {  // TODO: 2017/5/8 测试命令行提交 
-}
--- app/src/main/java/com/example/skd/myapp/sunkeding/sadasdasd.java
+++
@@ -1,8 +0,0 @@
-package com.example.skd.myapp.sunkeding;
-
-/**
- * Created by skd on 2017/4/5.
- */
-
-public class sadasdasd {
-}
