55848c44040b41bbf6c2cebcc2bd957429ec6c0c
keding <keding@leoao.com>
2017-05-12 16:44:55 +0800

    新增ViewHelperActivity

---
+++ app/src/main/java/com/example/skd/myapp/activitys/ViewHelperActivity.java
@@ -0,0 +1,59 @@
+package com.example.skd.myapp.activitys;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import com.nineoldandroids.view.ViewHelper;
+import com.example.skd.myapp.R;
+
+/**
+ * Created by skd on 2017/5/12.
+ */
+
+public class ViewHelperActivity extends FragmentActivity implements View.OnClickListener {
+    private View header_logo;
+    private float moveDistanceY = 25;// logo初始移动距离为10
+    private float moveDistanceX = 25;// logo初始移动距离为10
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewhelper);
+        initAllViews();
+    }
+
+    private void initAllViews() {
+        header_logo = findViewById(R.id.header_logo);
+        findViewById(R.id.btn_moveUp).setOnClickListener(this);
+        findViewById(R.id.btn_moveDown).setOnClickListener(this);
+        findViewById(R.id.btn_moveLeft).setOnClickListener(this);
+        findViewById(R.id.btn_moveRight).setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_moveDown :
+                moveDistanceY += 20;
+                ViewHelper.setTranslationY(header_logo, moveDistanceY);
+                break;
+
+            case R.id.btn_moveUp :
+                moveDistanceY -= 20;
+                ViewHelper.setTranslationY(header_logo, moveDistanceY);
+                break;
+
+            case R.id.btn_moveLeft :
+                moveDistanceX -= 20;
+                ViewHelper.setTranslationX(header_logo, moveDistanceX);
+                break;
+
+            case R.id.btn_moveRight :
+                moveDistanceX += 20;
+                ViewHelper.setTranslationX(header_logo, moveDistanceX);
+                break;
+
+            default :
+                break;
+        }
+    }
+}
