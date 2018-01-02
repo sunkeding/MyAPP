b1d745cff04e208e18fbd1bd6d3b4bbb42a9e68c
keding <keding@leoao.com>
2018-01-02 18:25:18 +0800

    查看Glide源码得到了获取当前线程是否在主线程的新方法

--- app/src/main/java/com/example/skd/myapp/ViewFlipperActivity.java
+++ app/src/main/java/com/example/skd/myapp/ViewFlipperActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.ImageView;
@@ -25,7 +26,6 @@ public class ViewFlipperActivity extends Activity {
         TextView tv = (TextView) findViewById(R.id.tv);
         final ViewFlipper vf = (ViewFlipper) findViewById(R.id.vf);
         Glide.with(this).load("https://img.leoao.com/2017/12/25/Fson-_gBATBXEAXRM57dWtDoxNxA.jpg").into(iv);
-        Log.d("ViewFlipperActivity", "System.currentTimeMillis():" + System.currentTimeMillis());
         CountDownTimer countDownTimer = new CountDownTimer(5000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -33,12 +33,10 @@ public class ViewFlipperActivity extends Activity {
 
             @Override
             public void onFinish() {
-                Log.d("ViewFlipperActivity", "System.currentTimeMillis():" + System.currentTimeMillis());
                 vf.setDisplayedChild(1);
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Log.d("ViewFlipperActivity", "System.currentTimeMillis():" + System.currentTimeMillis());
                         vf.setDisplayedChild(0);
 
                     }
@@ -47,5 +45,20 @@ public class ViewFlipperActivity extends Activity {
             }
         };
         countDownTimer.start();
+        Log.d("ViewFlipperActivity", "1->Looper.myLooper():" + Looper.myLooper());
+        Log.d("ViewFlipperActivity", "2->Looper.getMainLooper():" + Looper.getMainLooper());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("ViewFlipperActivity", "3->Looper.myLooper():" + Looper.myLooper());
+            }
+        }).start();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("ViewFlipperActivity", "4->Looper.myLooper():" + Looper.myLooper());
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/skd/myapp/activitys/mmmmtest.java
+++
@@ -1,9 +0,0 @@
-package com.example.skd.myapp.activitys;
-
-/**
- * Created by skd on 2017/6/5.
- */
-
-public class mmmmtest { // TODO: 2017/6/5  打了标签之后新增的文件
-    int id=0;// TODO: 2017/6/5 模拟发布版本之后的第二次修改提交 
-}
