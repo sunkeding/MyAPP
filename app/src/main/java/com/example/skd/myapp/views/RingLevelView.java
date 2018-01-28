9ad785ab0d071aee3b49fb68b2a5b806e9a52389
keding <keding@leoao.com>
2018-01-28 15:24:27 +0800

    圆环初稿

--- app/src/main/java/com/example/skd/myapp/views/RingLevelView.java
+++ app/src/main/java/com/example/skd/myapp/views/RingLevelView.java
@@ -23,6 +23,7 @@ public class RingLevelView extends ProgressBar {
     private Paint paint2;
     private int strokeWidth = 20;
     private float percent;
+    private Paint paint3;
 
 
     @Override
@@ -30,7 +31,7 @@ public class RingLevelView extends ProgressBar {
         this.progress = progress;
     }
 
-    double progress = 0.5;
+    double progress = 0.7;
 
     public RingLevelView(Context context) {
         this(context, null);
@@ -53,57 +54,40 @@ public class RingLevelView extends ProgressBar {
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeCap(Paint.Cap.ROUND);
-
         paint.setStrokeWidth(strokeWidth);
+
         paint2 = new Paint();
         paint2.setColor(Color.BLUE);
         paint2.setAntiAlias(true);
         paint2.setStyle(Paint.Style.STROKE);
         paint2.setStrokeCap(Paint.Cap.ROUND);
-
         paint2.setStrokeWidth(strokeWidth);
+
+        paint3 = new Paint();
+        paint3.setColor(Color.RED);
+        paint3.setAntiAlias(true);
+        paint3.setStyle(Paint.Style.STROKE);
+        paint3.setStrokeCap(Paint.Cap.ROUND);
+        paint3.setStrokeWidth(strokeWidth);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        canvas.drawRect(new RectF(0,0,400,400),paint);
-//        canvas.drawCircle(200,200,200-20,paint);
-//        canvas.drawArc(new RectF(0+20,0+20,400-20,400-20),0, (float) (progress*360),false,paint2);
-
-        final int restore = canvas.save();
-
         final int cx = getMeasuredWidth() / 2;
         final int cy = getMeasuredHeight() / 2;
         final int radius = getMeasuredWidth() / 2 - strokeWidth / 2;
         Log.d("RingLevelView", "cx:" + cx);
         Log.d("RingLevelView", "cy:" + cy);
         Log.d("RingLevelView", "radius:" + radius);
-
-
-        // TODO: 2018/1/19 圆弧画出来有点偏差
         canvas.save();
         canvas.rotate(-270, cx, cy); //画布沿着圆心转270度，为了画弧线的起始点在最下方
-//        canvas.drawRect(new RectF(0,0,getMeasuredWidth()+strokeWidth,getMeasuredWidth()+strokeWidth),paint2);
+        canvas.drawRect(new RectF(strokeWidth/2,strokeWidth/2,getMeasuredWidth()-strokeWidth/2,getMeasuredWidth()-strokeWidth/2),paint3);
         canvas.drawCircle(cx, cy, radius, paint);
-        canvas.drawArc(new RectF(0+strokeWidth,0+strokeWidth,getMeasuredWidth()-strokeWidth/2,getMeasuredWidth()-strokeWidth/2),0, (float) (progress*360),false,paint2);
+        canvas.drawArc(new RectF(strokeWidth/2,strokeWidth/2,getMeasuredWidth()-strokeWidth/2,getMeasuredWidth()-strokeWidth/2),0, (float) (progress*360),false,paint2);
         canvas.restore();
 
 
-
-        // TODO: 2018/1/19 此时控件设置300dp能够满足，但是设置100dp就有问题
-//        canvas.drawRect(new RectF(0, 0, 400, 400), paint2);
-//        canvas.drawCircle(200, 200, 200-strokeWidth, paint);
-//        canvas.drawArc(new RectF(0 + strokeWidth, 0 + strokeWidth, 400 - strokeWidth, 400 - strokeWidth), 0, (float) (progress * 360), false, paint2);
-
-
-
-
-
-
-//        canvas.drawRect(new RectF(0, 0, getMeasuredWidth(), getMeasuredWidth()), paint2);
-//        canvas.drawCircle(getMeasuredWidth()/2, getMeasuredWidth()/2, getMeasuredWidth()-strokeWidth, paint);
-//        canvas.drawArc(new RectF(0 + strokeWidth, 0 + strokeWidth, getMeasuredWidth() - strokeWidth, getMeasuredWidth() - strokeWidth), 0, (float) (progress * 360), false, paint2);
 
     }
 
